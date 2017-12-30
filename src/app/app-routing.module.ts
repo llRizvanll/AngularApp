18f7ced5f9b4c3e5c7f08045c92f235b8b006113
rizvan <torizvan@gmail.com>
2017-12-30 16:43:43 +0530

    added route and routeModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent} from './heroes/heroes.component';
+import { DashboardComponent} from './dashboard/dashboard.component';
+
+
+const routes : Routes = [
+  { path : 'heroes', component: HeroesComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path : 'dashboard', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
+  exports : [ RouterModule],
   declarations: []
 })
-export class AppRoutingModule { }
+
+
+
+export class AppRoutingModule { 
+    
+}
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,10 +1,10 @@
 import { Hero } from './hero';
 
 export const HEROES : Hero[] = [
-    { id : 1, name : 'A'},
-    { id : 2, name : 'B'},
-    { id : 3, name : 'C'},
-    { id : 4, name : 'D'},
-    { id : 5, name : 'E'},
-    { id : 6, name : 'F'}
+    { id : 1, name : 'IRON MAN'},
+    { id : 2, name : 'SUPER MAN'},
+    { id : 3, name : 'WONDER WOMAN'},
+    { id : 4, name : 'BAT MAN'},
+    { id : 5, name : 'AQUA MAN'},
+    { id : 6, name : 'SPIDER MAN'}
 ]
